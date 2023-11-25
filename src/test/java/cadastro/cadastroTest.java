57f80882775846c37252a3dea2308ecf8cbb4cdf
Leonardo Freitas <leonardosilvadefreitas@hotmail.com>
2023-11-25 00:59:22 -0300

    Plugin Jacoco

--- src/test/java/cadastro/cadastroTest.java
+++ src/test/java/cadastro/cadastroTest.java
@@ -7,15 +7,14 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class cadastroTest {
+public class cadastroTest{
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/q/health")
           .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .statusCode(200);
     }
 
 }
